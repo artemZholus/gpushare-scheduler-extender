676596256c97b755eedd536c2f727dfd65c911fa
Artem Zholus <artem.zholus@gmail.com>
2019-06-03 15:36:09 +0000

    local version

--- deployer/docker/device-plugin-evict/dp-evict/dp-evict-on-host.sh
+++ deployer/docker/device-plugin-evict/dp-evict/dp-evict-on-host.sh
@@ -4,11 +4,11 @@ set -e -x
 
 backup_dir="/etc/kubernetes/manifests-backup"
 
-public::common::log() {
+log() {
 	echo $(date +"[%Y%m%d %H:%M:%S]: ") $1
 }
 
-public::evict::gpu-device-plugin() {
+gpu-device-plugin() {
 	dir=/etc/kubernetes/manifests/
 
 	if [  -f /etc/kubernetes/manifests/nvidia-device-plugin.yml ]; then
@@ -16,13 +16,13 @@ public::evict::gpu-device-plugin() {
 			mkdir -p $backup_dir
 			mv /etc/kubernetes/manifests/nvidia-device-plugin.yml $backup_dir
 		else
-			public::common::log "Skip removing nvidia-device-plugin.yml, because it doesn't exist."
+			log "Skip removing nvidia-device-plugin.yml, because it doesn't exist."
 	fi
 }
 
 main() {
 
-	public::evict::gpu-device-plugin
+	gpu-device-plugin
 
 	touch /ready
 }
--- deployer/docker/schd-extender/schd-extender/install-sched-extender-on-host.sh
+++ deployer/docker/schd-extender/schd-extender/install-sched-extender-on-host.sh
@@ -8,37 +8,37 @@ backup_dir="/etc/kubernetes/manifests-backup"
 
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
-public::common::log() {
+log() {
 	echo $(date +"[%Y%m%d %H:%M:%S]: ") $1
 }
 
-public::deployer::sche-policy-config() {
+sche-policy-config() {
 	
 	mkdir -p $backup_dir
 
     if [ ! -f $backup_dir/kube-scheduler.ori.yaml ];then
         cp /etc/kubernetes/manifests/kube-scheduler.yaml $backup_dir/kube-scheduler.ori.yaml
-	    public::common::log "Backup $backup_dir/kube-scheduler.ori.yaml"
+	    log "Backup $backup_dir/kube-scheduler.ori.yaml"
     else
 	    cp /etc/kubernetes/manifests/kube-scheduler.yaml $backup_dir/kube-scheduler-$TIMESTAMP.yaml
-	    public::common::log "Backup $backup_dir/kube-scheduler-$TIMESTAMP.yaml"
+	    log "Backup $backup_dir/kube-scheduler-$TIMESTAMP.yaml"
     fi
 
     if [ ! -f $backup_dir/scheduler-policy-config.ori.json ];then
         if [ -f /etc/kubernetes/scheduler-policy-config.json ];then
             cp /etc/kubernetes/scheduler-policy-config.json $backup_dir/scheduler-policy-config.ori.json
-            public::common::log "Backup $backup_dir/scheduler-policy-config.ori.json"
+            log "Backup $backup_dir/scheduler-policy-config.ori.json"
         fi
     else
         if [ -f /etc/kubernetes/scheduler-policy-config.json ];then
             cp /etc/kubernetes/scheduler-policy-config.json $backup_dir/scheduler-policy-config-$TIMESTAMP.json
-            public::common::log "Backup $backup_dir/scheduler-policy-config-$TIMESTAMP.json"
+            log "Backup $backup_dir/scheduler-policy-config-$TIMESTAMP.json"
         fi
     fi
 
-	public::common::log "Configure shceduler extender"
-	cp -f /usr/local/k8s-schd-extender/scheduler-policy-config.json /etc/kubernetes/scheduler-policy-config.json
-    sed -i 's/127.0.0.1/'"${NODE_IP}"'/g' /etc/kubernetes/scheduler-policy-config.json
+	log "Configure shceduler extender"
+	cp -f ./config/scheduler-policy-config.json /etc/kubernetes/scheduler-policy-config.json
+    #sed -i 's/127.0.0.1/'"${NODE_IP}"'/g' /etc/kubernetes/scheduler-policy-config.json
     if ! grep 'deployment.kubernetes.io/revision' $dir/kube-scheduler.yaml; then
         sed -i '/scheduler.alpha.kubernetes.io\/critical-pod/a \    deployment.kubernetes.io/revision: "'"${TIMESTAMP}"'"' $dir/kube-scheduler.yaml
     else
@@ -49,19 +49,19 @@ public::deployer::sche-policy-config() {
 	if ! grep 'policy-config-file=/etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
 		sed -i "/- kube-scheduler/a\ \ \ \ - --policy-config-file=/etc/kubernetes/scheduler-policy-config.json" $dir/kube-scheduler.yaml
 	else
-		public::common::log "Skip the kube-scheduler config, because it's already configured extender."
+		log "Skip the kube-scheduler config, because it's already configured extender."
 	fi
 	# add scheduler config policy volumeMounts
 	if ! grep 'mountPath: /etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
 		sed -i "/  volumeMounts:/a\ \ \ \ - mountPath: /etc/kubernetes/scheduler-policy-config.json\n      name: scheduler-policy-config\n      readOnly: true" $dir/kube-scheduler.yaml
 	else
-		public::common::log "Skip the scheduler-policy-config mountPath, because it's already configured extender."
+		log "Skip the scheduler-policy-config mountPath, because it's already configured extender."
 	fi
 	# add scheduler config policy volumes
 	if ! grep 'path: /etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
 		sed -i "/  volumes:/a \  - hostPath:\n      path: /etc/kubernetes/scheduler-policy-config.json\n      type: FileOrCreate\n    name: scheduler-policy-config" $dir/kube-scheduler.yaml
 	else
-		public::common::log "Skip the scheduler-policy-config volumes, because it's already configured extender."
+		log "Skip the scheduler-policy-config volumes, because it's already configured extender."
 	fi
 }
 
@@ -78,7 +78,7 @@ main() {
 			shift
 			;;
 		*)
-			public::common::log "unkonw option [$key]"
+			log "unkonw option [$key]"
 			exit 1
 			;;
 		esac
@@ -87,11 +87,11 @@ main() {
 
 	if [ "$KUBE_VERSION" == "" ]; then
 		# Using default cidr.
-		public::common::log "KUBE_VERSION $KUBE_VERSION is not set."
+		log "KUBE_VERSION $KUBE_VERSION is not set."
 		exit 1
 	fi
 
-	public::deployer::sche-policy-config
+	sche-policy-config
 
 	touch /ready
 }
