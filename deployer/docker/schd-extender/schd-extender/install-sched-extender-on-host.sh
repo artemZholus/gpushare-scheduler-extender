15e3a2547ecc53583970a38bc1007abd096e7b85
Artem Zholus <artem.zholus@gmail.com>
2019-06-03 15:40:40 +0000

    sub tabs

--- deployer/docker/schd-extender/schd-extender/install-sched-extender-on-host.sh
+++ deployer/docker/schd-extender/schd-extender/install-sched-extender-on-host.sh
@@ -9,19 +9,19 @@ backup_dir="/etc/kubernetes/manifests-backup"
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
 log() {
-	echo $(date +"[%Y%m%d %H:%M:%S]: ") $1
+    echo $(date +"[%Y%m%d %H:%M:%S]: ") $1
 }
 
 sche-policy-config() {
-	
-	mkdir -p $backup_dir
+    
+    mkdir -p $backup_dir
 
     if [ ! -f $backup_dir/kube-scheduler.ori.yaml ];then
         cp /etc/kubernetes/manifests/kube-scheduler.yaml $backup_dir/kube-scheduler.ori.yaml
-	    log "Backup $backup_dir/kube-scheduler.ori.yaml"
+        log "Backup $backup_dir/kube-scheduler.ori.yaml"
     else
-	    cp /etc/kubernetes/manifests/kube-scheduler.yaml $backup_dir/kube-scheduler-$TIMESTAMP.yaml
-	    log "Backup $backup_dir/kube-scheduler-$TIMESTAMP.yaml"
+        cp /etc/kubernetes/manifests/kube-scheduler.yaml $backup_dir/kube-scheduler-$TIMESTAMP.yaml
+        log "Backup $backup_dir/kube-scheduler-$TIMESTAMP.yaml"
     fi
 
     if [ ! -f $backup_dir/scheduler-policy-config.ori.json ];then
@@ -36,8 +36,8 @@ sche-policy-config() {
         fi
     fi
 
-	log "Configure shceduler extender"
-	cp -f ./config/scheduler-policy-config.json /etc/kubernetes/scheduler-policy-config.json
+    log "Configure shceduler extender"
+    cp -f ./config/scheduler-policy-config.json /etc/kubernetes/scheduler-policy-config.json
     #sed -i 's/127.0.0.1/'"${NODE_IP}"'/g' /etc/kubernetes/scheduler-policy-config.json
     if ! grep 'deployment.kubernetes.io/revision' $dir/kube-scheduler.yaml; then
         sed -i '/scheduler.alpha.kubernetes.io\/critical-pod/a \    deployment.kubernetes.io/revision: "'"${TIMESTAMP}"'"' $dir/kube-scheduler.yaml
@@ -46,54 +46,54 @@ sche-policy-config() {
         sed -i 's#deployment.kubernetes.io/revision:.*#deployment.kubernetes.io/revision: "'"${TIMESTAMP}"'"#' $dir/kube-scheduler.yaml
     fi
     
-	if ! grep 'policy-config-file=/etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
-		sed -i "/- kube-scheduler/a\ \ \ \ - --policy-config-file=/etc/kubernetes/scheduler-policy-config.json" $dir/kube-scheduler.yaml
-	else
-		log "Skip the kube-scheduler config, because it's already configured extender."
-	fi
-	# add scheduler config policy volumeMounts
-	if ! grep 'mountPath: /etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
-		sed -i "/  volumeMounts:/a\ \ \ \ - mountPath: /etc/kubernetes/scheduler-policy-config.json\n      name: scheduler-policy-config\n      readOnly: true" $dir/kube-scheduler.yaml
-	else
-		log "Skip the scheduler-policy-config mountPath, because it's already configured extender."
-	fi
-	# add scheduler config policy volumes
-	if ! grep 'path: /etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
-		sed -i "/  volumes:/a \  - hostPath:\n      path: /etc/kubernetes/scheduler-policy-config.json\n      type: FileOrCreate\n    name: scheduler-policy-config" $dir/kube-scheduler.yaml
-	else
-		log "Skip the scheduler-policy-config volumes, because it's already configured extender."
-	fi
+    if ! grep 'policy-config-file=/etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
+        sed -i "/- kube-scheduler/a\ \ \ \ - --policy-config-file=/etc/kubernetes/scheduler-policy-config.json" $dir/kube-scheduler.yaml
+    else
+        log "Skip the kube-scheduler config, because it's already configured extender."
+    fi
+    # add scheduler config policy volumeMounts
+    if ! grep 'mountPath: /etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
+        sed -i "/  volumeMounts:/a\ \ \ \ - mountPath: /etc/kubernetes/scheduler-policy-config.json\n      name: scheduler-policy-config\n      readOnly: true" $dir/kube-scheduler.yaml
+    else
+        log "Skip the scheduler-policy-config mountPath, because it's already configured extender."
+    fi
+    # add scheduler config policy volumes
+    if ! grep 'path: /etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
+        sed -i "/  volumes:/a \  - hostPath:\n      path: /etc/kubernetes/scheduler-policy-config.json\n      type: FileOrCreate\n    name: scheduler-policy-config" $dir/kube-scheduler.yaml
+    else
+        log "Skip the scheduler-policy-config volumes, because it's already configured extender."
+    fi
 }
 
 main() {
-	while
-		[[ $# -gt 0 ]]
-	do
-		key="$1"
+    while
+        [[ $# -gt 0 ]]
+    do
+        key="$1"
 
-		case $key in
-		--version)
-			KUBE_VERSION=$2
-			export KUBE_VERSION=${KUBE_VERSION:1}
-			shift
-			;;
-		*)
-			log "unkonw option [$key]"
-			exit 1
-			;;
-		esac
-		shift
-	done
+        case $key in
+        --version)
+            KUBE_VERSION=$2
+            export KUBE_VERSION=${KUBE_VERSION:1}
+            shift
+            ;;
+        *)
+            log "unkonw option [$key]"
+            exit 1
+            ;;
+        esac
+        shift
+    done
 
-	if [ "$KUBE_VERSION" == "" ]; then
-		# Using default cidr.
-		log "KUBE_VERSION $KUBE_VERSION is not set."
-		exit 1
-	fi
+    if [ "$KUBE_VERSION" == "" ]; then
+        # Using default cidr.
+        log "KUBE_VERSION $KUBE_VERSION is not set."
+        exit 1
+    fi
 
-	sche-policy-config
+    sche-policy-config
 
-	touch /ready
+    touch /ready
 }
 
 main "$@"
